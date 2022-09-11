c9d02b8df706090ebc85aff412c4b8db5c989bb5
John Leraas <102673161+johnleraas@users.noreply.github.com>
2022-09-11 09:06:01 -0400

    Update misc_functions.py
    Added initial fns

--- misc_functions.py
+++ misc_functions.py
@@ -8,3 +8,28 @@ def add_two(x):
 
 def divide_three(x):
     return x/3
+
+# Ensure proper format
+def ensure_categorical(df, col_list):
+  for col in col_list:
+    df[col]=df[col].astype('category')
+  return df
+
+# Load File
+def load_file(file):
+  df = pd.read_csv(file, index_col=0)
+
+  # Ensure Date proper format
+  df['Date']=pd.to_datetime(df['Date'])
+
+  df = df.set_index('Date')
+
+  # Select Categorical Columns and Assign
+  cat_cols = ['dow', 'fed_holiday', 'is_wknd', 'is_month_start', 'is_month_end', 'wkday_fedholiday_lag1']
+  df = ensure_categorical(df=df, col_list=cat_cols)
+
+  # Select all "Holiday Lag" columns
+  cat_cols = [col for col in df.columns.to_list() if col.startswith('fed_holiday_lag')]
+  df = ensure_categorical(df=df, col_list=cat_cols)
+
+  return df
